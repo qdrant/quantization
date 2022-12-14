3789fe318bdad0caa489c04c96fa569c7935182d
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2022-12-14 12:06:30 +0100

    fix bugs

--- quantization/src/sse_scorer.rs
+++ quantization/src/sse_scorer.rs
@@ -9,7 +9,6 @@ pub struct SseScorer<'a> {
 
 impl Scorer for SseScorer<'_> {
     //#[inline]
-
     fn score_point(&self, point: usize) -> f32 {
         unsafe {
             let v = self.lut.encoded_vectors.get(point as usize);
@@ -25,14 +24,15 @@ impl Scorer for SseScorer<'_> {
                     let c2 = codes & 0x0F;
                     codes >>= 8;
 
-                    let alpha = *(lut_ptr as *const u32);
+                    let alpha1 = *(lut_ptr as *const u32);
                     lut_ptr = lut_ptr.add(4);
-                    sum += alpha * (*lut_ptr.add(c1 as usize) as u32);
+                    let alpha2 = *(lut_ptr as *const u32);
+                    lut_ptr = lut_ptr.add(4);
+
+                    sum += alpha1 * (*lut_ptr.add(c1 as usize) as u32);
                     lut_ptr = lut_ptr.add(16);
 
-                    let alpha = *(lut_ptr as *const u32);
-                    lut_ptr = lut_ptr.add(4);
-                    sum += alpha * (*lut_ptr.add(c2 as usize) as u32);
+                    sum += alpha2 * (*lut_ptr.add(c2 as usize) as u32);
                     lut_ptr = lut_ptr.add(16);
                 }
             }
@@ -44,27 +44,27 @@ impl Scorer for SseScorer<'_> {
         unsafe {
             scores.fill(0.0);
             let vector_size = self.vector_size;
-            let mut lut_ptr = self.lut.centroid_distances.as_ptr();
-
             let distances_mask = _mm_set_epi32(0xFF, 0xFF, 0xFF, 0xFF);
             let low_4bits_mask = _mm_set1_epi8(0x0F);
             for (point_ids, scores) in points
                 .chunks_exact(CENTROIDS_COUNT)
                 .zip(scores.chunks_exact_mut(CENTROIDS_COUNT))
             {
+                let mut lut_ptr = self.lut.centroid_distances.as_ptr();
+
                 let mut sum1: __m128i = _mm_setzero_si128();
                 let mut sum2: __m128i = _mm_setzero_si128();
                 let mut sum3: __m128i = _mm_setzero_si128();
                 let mut sum4: __m128i = _mm_setzero_si128();
 
-                const SUBVECTOR_SIZE: usize = 32;
+                const SUBVECTOR_SIZE: usize = 16;
                 for sv in 0..vector_size / SUBVECTOR_SIZE {
                     for i in 0..CENTROIDS_COUNT {
                         std::ptr::copy_nonoverlapping(
-                            self.lut.encoded_vectors.get_ptr(point_ids[i]),
+                            self.lut.encoded_vectors.get_ptr(point_ids[i]).add(sv * SUBVECTOR_SIZE),
                             self.points_data
                                 .as_mut_ptr()
-                                .add(i * vector_size + sv * SUBVECTOR_SIZE),
+                                .add(i * SUBVECTOR_SIZE),
                             SUBVECTOR_SIZE,
                         );
                     }
@@ -73,11 +73,11 @@ impl Scorer for SseScorer<'_> {
                     for _ in 0..SUBVECTOR_SIZE {
                         let alpha1 = *(lut_ptr as *const i32);
                         lut_ptr = lut_ptr.add(4);
-                        let lut1 = _mm_loadu_si128(lut_ptr as *const __m128i);
-                        lut_ptr = lut_ptr.add(16);
-
                         let alpha2 = *(lut_ptr as *const i32);
                         lut_ptr = lut_ptr.add(4);
+
+                        let lut1 = _mm_loadu_si128(lut_ptr as *const __m128i);
+                        lut_ptr = lut_ptr.add(16);
                         let lut2 = _mm_loadu_si128(lut_ptr as *const __m128i);
                         lut_ptr = lut_ptr.add(16);
 
@@ -105,23 +105,23 @@ impl Scorer for SseScorer<'_> {
                         let dists = _mm_shuffle_epi8(lut2, codes_low);
 
                         let dists_part = _mm_and_si128(dists, distances_mask);
-                        let dists_part = _mm_mullo_epi16(dists_part, alpha2);
-                        sum1 = _mm_adds_epu16(sum1, dists_part);
+                        let dists_part = _mm_mullo_epi32(dists_part, alpha2);
+                        sum1 = _mm_add_epi32(sum1, dists_part);
 
                         let dists_part = _mm_srli_epi32(dists, 8);
                         let dists_part = _mm_and_si128(dists_part, distances_mask);
-                        let dists_part = _mm_mullo_epi16(dists_part, alpha2);
-                        sum2 = _mm_adds_epu16(sum2, dists_part);
+                        let dists_part = _mm_mullo_epi32(dists_part, alpha2);
+                        sum2 = _mm_add_epi32(sum2, dists_part);
 
                         let dists_part = _mm_srli_epi32(dists, 16);
                         let dists_part = _mm_and_si128(dists_part, distances_mask);
-                        let dists_part = _mm_mullo_epi16(dists_part, alpha2);
-                        sum3 = _mm_adds_epu16(sum3, dists_part);
+                        let dists_part = _mm_mullo_epi32(dists_part, alpha2);
+                        sum3 = _mm_add_epi32(sum3, dists_part);
 
                         let dists_part = _mm_srli_epi32(dists, 24);
                         let dists_part = _mm_and_si128(dists_part, distances_mask);
-                        let dists_part = _mm_mullo_epi16(dists_part, alpha2);
-                        sum4 = _mm_adds_epu16(sum4, dists_part);
+                        let dists_part = _mm_mullo_epi32(dists_part, alpha2);
+                        sum4 = _mm_add_epi32(sum4, dists_part);
 
                         let codes_shft = _mm_srli_epi16(codes, 4);
                         let codes_high = _mm_and_si128(codes_shft, low_4bits_mask);
@@ -129,51 +129,68 @@ impl Scorer for SseScorer<'_> {
                         let dists = _mm_shuffle_epi8(lut1, codes_high);
 
                         let dists_part = _mm_and_si128(dists, distances_mask);
-                        let dists_part = _mm_mullo_epi16(dists_part, alpha1);
-                        sum1 = _mm_adds_epu16(sum1, dists_part);
+                        let dists_part = _mm_mullo_epi32(dists_part, alpha1);
+                        sum1 = _mm_add_epi32(sum1, dists_part);
 
                         let dists_part = _mm_srli_epi32(dists, 8);
                         let dists_part = _mm_and_si128(dists_part, distances_mask);
-                        let dists_part = _mm_mullo_epi16(dists_part, alpha1);
-                        sum2 = _mm_adds_epu16(sum2, dists_part);
+                        let dists_part = _mm_mullo_epi32(dists_part, alpha1);
+                        sum2 = _mm_add_epi32(sum2, dists_part);
 
                         let dists_part = _mm_srli_epi32(dists, 16);
                         let dists_part = _mm_and_si128(dists_part, distances_mask);
-                        let dists_part = _mm_mullo_epi16(dists_part, alpha1);
-                        sum3 = _mm_adds_epu16(sum3, dists_part);
+                        let dists_part = _mm_mullo_epi32(dists_part, alpha1);
+                        sum3 = _mm_add_epi32(sum3, dists_part);
 
                         let dists_part = _mm_srli_epi32(dists, 24);
                         let dists_part = _mm_and_si128(dists_part, distances_mask);
-                        let dists_part = _mm_mullo_epi16(dists_part, alpha1);
-                        sum4 = _mm_adds_epu16(sum4, dists_part);
+                        let dists_part = _mm_mullo_epi32(dists_part, alpha1);
+                        sum4 = _mm_add_epi32(sum4, dists_part);
 
                         points_ptr = points_ptr.add(1);
                     }
                 }
 
                 let sum1 = _mm_cvtepi32_ps(sum1);
-                scores[0] = f32::from_bits(_mm_extract_ps(sum1, 0) as u32) * self.lut.alpha + self.lut.offset;
-                scores[1] = f32::from_bits(_mm_extract_ps(sum1, 1) as u32) * self.lut.alpha + self.lut.offset;
-                scores[2] = f32::from_bits(_mm_extract_ps(sum1, 2) as u32) * self.lut.alpha + self.lut.offset;
-                scores[3] = f32::from_bits(_mm_extract_ps(sum1, 3) as u32) * self.lut.alpha + self.lut.offset;
-
                 let sum2 = _mm_cvtepi32_ps(sum2);
-                scores[4] = f32::from_bits(_mm_extract_ps(sum2, 0) as u32) * self.lut.alpha + self.lut.offset;
-                scores[5] = f32::from_bits(_mm_extract_ps(sum2, 1) as u32) * self.lut.alpha + self.lut.offset;
-                scores[6] = f32::from_bits(_mm_extract_ps(sum2, 2) as u32) * self.lut.alpha + self.lut.offset;
-                scores[7] = f32::from_bits(_mm_extract_ps(sum2, 3) as u32) * self.lut.alpha + self.lut.offset;
-
                 let sum3 = _mm_cvtepi32_ps(sum3);
-                scores[8] = f32::from_bits(_mm_extract_ps(sum3, 0) as u32) * self.lut.alpha + self.lut.offset;
-                scores[9] = f32::from_bits(_mm_extract_ps(sum3, 1) as u32) * self.lut.alpha + self.lut.offset;
-                scores[10] = f32::from_bits(_mm_extract_ps(sum3, 2) as u32) * self.lut.alpha + self.lut.offset;
-                scores[11] = f32::from_bits(_mm_extract_ps(sum3, 3) as u32) * self.lut.alpha + self.lut.offset;
-            
                 let sum4 = _mm_cvtepi32_ps(sum4);
-                scores[12] = f32::from_bits(_mm_extract_ps(sum4, 0) as u32) * self.lut.alpha + self.lut.offset;
-                scores[13] = f32::from_bits(_mm_extract_ps(sum4, 1) as u32) * self.lut.alpha + self.lut.offset;
-                scores[14] = f32::from_bits(_mm_extract_ps(sum4, 2) as u32) * self.lut.alpha + self.lut.offset;
-                scores[15] = f32::from_bits(_mm_extract_ps(sum4, 3) as u32) * self.lut.alpha + self.lut.offset;
+
+                scores[15] = f32::from_bits(_mm_extract_ps(sum1, 0) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[11] = f32::from_bits(_mm_extract_ps(sum1, 1) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[7] = f32::from_bits(_mm_extract_ps(sum1, 2) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[3] = f32::from_bits(_mm_extract_ps(sum1, 3) as u32) * self.lut.alpha
+                    + self.lut.offset;
+
+                scores[14] = f32::from_bits(_mm_extract_ps(sum2, 0) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[10] = f32::from_bits(_mm_extract_ps(sum2, 1) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[6] = f32::from_bits(_mm_extract_ps(sum2, 2) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[2] = f32::from_bits(_mm_extract_ps(sum2, 3) as u32) * self.lut.alpha
+                    + self.lut.offset;
+
+                scores[13] = f32::from_bits(_mm_extract_ps(sum3, 0) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[9] = f32::from_bits(_mm_extract_ps(sum3, 1) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[5] = f32::from_bits(_mm_extract_ps(sum3, 2) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[1] = f32::from_bits(_mm_extract_ps(sum3, 3) as u32) * self.lut.alpha
+                    + self.lut.offset;
+
+                scores[12] = f32::from_bits(_mm_extract_ps(sum4, 0) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[8] = f32::from_bits(_mm_extract_ps(sum4, 1) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[4] = f32::from_bits(_mm_extract_ps(sum4, 2) as u32) * self.lut.alpha
+                    + self.lut.offset;
+                scores[0] = f32::from_bits(_mm_extract_ps(sum4, 3) as u32) * self.lut.alpha
+                    + self.lut.offset;
             }
         }
     }
@@ -186,5 +203,70 @@ impl<'a> From<CompressedLookupTable<'a>> for SseScorer<'a> {
             points_data: vec![0u8; lut.encoded_vectors.vector_size * 16],
             lut,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{encoded_vectors::EncodedVectors, simple_scorer::SimpleScorer};
+    use rand::{rngs::StdRng, Rng, SeedableRng};
+    use super::*;
+
+    #[test]
+    fn test_sse_scoring() {
+        let count = 64;
+        let dim = 256;
+
+        let mut rng = StdRng::seed_from_u64(42);
+        let mut encoded = EncodedVectors {
+            data: vec![0; count * dim / 2],
+            vector_size: dim / 2,
+            centroids: vec![], //not needed
+            chunks: vec![1; dim],
+            dim,
+        };
+
+        for i in 0..count {
+            for j in 0..dim / 2 {
+                encoded.data[i * dim / 2 + j] = (rng.gen::<u8>() % 16) as u8;
+            }
+        }
+
+        let mut centroid_distances: Vec<u8> = vec![];
+        for i in 0..dim / 2 {
+            let alpha1: i32 = 20 * i as i32;
+            centroid_distances.extend_from_slice(&alpha1.to_ne_bytes());
+            let alpha2: i32 = 20 * i as i32 + 1;
+            centroid_distances.extend_from_slice(&alpha2.to_ne_bytes());
+            for _ in 0..16 {
+                centroid_distances.push(rng.gen::<u8>());
+            }
+            for _ in 0..16 {
+                centroid_distances.push(rng.gen::<u8>());
+            }
+        }
+
+        let mut simple_scorer: SimpleScorer = CompressedLookupTable {
+            encoded_vectors: &encoded,
+            centroid_distances: centroid_distances.clone(),
+            alpha: 1.0,
+            offset: 1.0,
+        }.into();
+
+        let mut sse_scorer: SseScorer = CompressedLookupTable {
+            encoded_vectors: &encoded,
+            centroid_distances: centroid_distances.clone(),
+            alpha: 1.0,
+            offset: 1.0,
+        }.into();
+
+        let points: Vec<usize> = (0..count).collect();
+        let mut scores_simple: Vec<f32> = vec![0.0; count];
+        simple_scorer.score_points(&points, &mut scores_simple);
+
+        let mut scores_sse: Vec<f32> = vec![0.0; count];
+        sse_scorer.score_points(&points, &mut scores_sse);
+
+        assert_eq!(scores_simple, scores_sse);
     }
 }
