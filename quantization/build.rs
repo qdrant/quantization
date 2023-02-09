0637cdaa85a63ed0ce5ed693898cf1a8c1180412
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-02-09 18:42:37 +0000

    add msvc flags

--- quantization/build.rs
+++ quantization/build.rs
@@ -4,7 +4,14 @@ fn main() {
     #[cfg(target_arch = "x86_64")]
     {
         builder.file("cpp/avx2.c");
-        builder.flag("-march=haswell");
+        if builder.get_compiler().is_like_msvc() {
+            builder.flag("/arch:AVX");
+            builder.flag("/arch:AVX2");
+            builder.flag("/arch:SSE");
+            builder.flag("/arch:SSE2");
+        } else {
+            builder.flag("-march=haswell");
+        }
     }
 
     #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
