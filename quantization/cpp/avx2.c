747a82bbed99d86db3d17b7975c07320600f804f
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-02-09 19:23:00 +0000

    use immintrin header

--- quantization/cpp/avx2.c
+++ quantization/cpp/avx2.c
@@ -1,14 +1,5 @@
 #include <stdint.h>
-#include <mmintrin.h>  // MMX
-#include <xmmintrin.h> // SSE
-#include <emmintrin.h> // SSE2
-#include <pmmintrin.h> // SSE3
-#include <tmmintrin.h> // SSSE3
-#include <smmintrin.h> // SSE4.1
-#include <nmmintrin.h> // SSE4.2
-#include <ammintrin.h> // SSE4A
-#include <wmmintrin.h> // AES
-#include <immintrin.h> // AVX, AVX2, FMA
+#include <immintrin.h>
 
 #include "export_macro.h"
 
