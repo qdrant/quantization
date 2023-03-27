e2e733a1573ca024a2e4afae30758656df640c66
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-03-27 15:24:59 +0000

    test pq and dump to picture

--- quantization/src/kmeans.rs
+++ quantization/src/kmeans.rs
@@ -47,7 +47,7 @@ impl KMeans {
             vector_division,
         };
 
-        for _ in 0..30 {
+        for _ in 0..10 {
             println!("KMeans Iteration");
             kmeans.update_indexes(data.clone(), storage_builder);
             if kmeans.update_centroids(data.clone(), storage_builder) {
@@ -55,7 +55,8 @@ impl KMeans {
             }
         }
 
-        kmeans.update_indexes(data, storage_builder);
+        kmeans.update_indexes(data.clone(), storage_builder);
+        //kmeans.build_image(data, storage_builder, min, max);
         Ok(kmeans)
     }
 
@@ -67,25 +68,36 @@ impl KMeans {
         println!("Update Centroids");
         let mut byte_index = 0;
         let mut centroids_counter =
-            vec![vec![0usize; self.vector_division[0].len()]; self.centroids.len()];
+            vec![vec![0usize; self.vector_division.len()]; self.centroids.len()];
         let mut centroids_acc = vec![vec![0.0_f64; self.centroids[0].len()]; self.centroids.len()];
+        let mut rand_vectors = Vec::with_capacity(self.centroids.len());
         for vector_data in data.into_iter() {
+            if rand_vectors.len() < self.centroids.len() {
+                rand_vectors.push(vector_data);
+            }
             for (i, range) in self.vector_division.iter().enumerate() {
                 let subvector_data = &vector_data[range.clone()];
                 let centroid_index = storage_builder.get(byte_index) as usize;
-                let centroid_data = &mut self.centroids[centroid_index][range.clone()];
+                let centroid_data = &mut centroids_acc[centroid_index][range.clone()];
                 for (c, v) in centroid_data.iter_mut().zip(subvector_data.iter()) {
-                    *c += v;
+                    *c += *v as f64;
                 }
                 centroids_counter[centroid_index][i] += 1;
                 byte_index += 1;
             }
         }
+//        println!("centroids_acc {:?}", centroids_acc);
+//        println!("centroids_counter {:?}", centroids_counter);
         for (centroid_index, centroid) in centroids_acc.iter_mut().enumerate() {
             for (i, range) in self.vector_division.iter().enumerate() {
                 let centroid_data = &mut centroid[range.clone()];
-                for c in centroid_data.iter_mut() {
-                    *c /= centroids_counter[centroid_index][i] as f64;
+                let rand_vector = &rand_vectors[centroid_index][range.clone()];
+                for (c, r) in centroid_data.iter_mut().zip(rand_vector.iter()) {
+                    if centroids_counter[centroid_index][i] == 0 {
+                        *c = *r as f64;
+                    } else {
+                        *c /= centroids_counter[centroid_index][i] as f64;
+                    }
                 }
             }
         }
@@ -102,9 +114,10 @@ impl KMeans {
         storage_builder: &mut impl EncodedStorageBuilder<TStorage>,
     ) {
         println!("Update Indexes");
+        //println!("Vector division {:?}", self.vector_division);
         let mut byte_index = 0;
         for vector_data in data.into_iter() {
-            for range in &self.vector_division {
+            for range in self.vector_division.iter() {
                 let subvector_data = &vector_data[range.clone()];
                 let mut min_distance = f32::MAX;
                 let mut min_centroid_index = 0;
@@ -125,4 +138,64 @@ impl KMeans {
             }
         }
     }
+
+/*
+    fn build_image<'a, TStorage: EncodedStorage>(
+        &self,
+        data: impl IntoIterator<Item = &'a [f32]> + Clone,
+        storage_builder: &mut impl EncodedStorageBuilder<TStorage>,
+        min: f32,
+        max: f32,
+    ) {
+        //println!("All centroids {:?}", self.centroids);
+        println!("Build Image");
+        let colors_r: Vec<_> = (0..256).map(|_| rand::random::<u8>()).collect();
+        let colors_g: Vec<_> = (0..256).map(|_| rand::random::<u8>()).collect();
+        let colors_b: Vec<_> = (0..256).map(|_| rand::random::<u8>()).collect();
+        for (range_i, range) in self.vector_division.iter().enumerate() {
+            if range.len() < 2 {
+                continue;
+            }
+
+            //println!("Range {:?}", range);
+            let mut centroids_counter = (0..self.centroids.len())
+                .map(|_| 0usize)
+                .collect::<Vec<_>>();
+
+            let imgx = 1000;
+            let imgy = 1000;
+            let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+            for (_x, _y, pixel) in imgbuf.enumerate_pixels_mut() {
+                *pixel = image::Rgb([255u8, 255u8, 255u8]);
+            }
+
+            for (i, vector_data) in data.clone().into_iter().enumerate() {
+                let subvector_data = &vector_data[range.clone()];
+                let storage_builder_index = self.vector_division.len() * i + range_i;
+                let centroid_index = storage_builder.get(storage_builder_index) as usize;
+                centroids_counter[centroid_index] += 1;
+                let x = (((subvector_data[0] - min) / (max - min)) * imgx as f32).clamp(0., imgx as f32 - 1.0) as u32;
+                let y = (((subvector_data[1] - min) / (max - min)) * imgy as f32).clamp(0., imgy as f32 - 1.0) as u32;
+                *imgbuf.get_pixel_mut(x, y) = image::Rgb([
+                    colors_r[centroid_index],
+                    colors_g[centroid_index],
+                    colors_b[centroid_index],
+                ]);
+            }
+
+            for centroid in &self.centroids {
+                let subvector_data = &centroid[range.clone()];
+                let x = (((subvector_data[0] - min) / (max - min)) * imgx as f32).clamp(0., imgx as f32 - 2.0) as u32;
+                let y = (((subvector_data[1] - min) / (max - min)) * imgy as f32).clamp(0., imgy as f32 - 2.0) as u32;
+                *imgbuf.get_pixel_mut(x, y) = image::Rgb([255u8, 0u8, 0u8]);
+                *imgbuf.get_pixel_mut(x + 1, y) = image::Rgb([255u8, 0u8, 0u8]);
+                *imgbuf.get_pixel_mut(x + 1, y + 1) = image::Rgb([255u8, 0u8, 0u8]);
+                *imgbuf.get_pixel_mut(x, y + 1) = image::Rgb([255u8, 0u8, 0u8]);
+            }
+
+            //println!("Centroids distribution {:?}", centroids_counter);
+            imgbuf.save(&format!("target/kmeans-{range_i}.png")).unwrap();
+        }
+    }
+*/
 }
