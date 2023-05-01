0f537706f2e98afdb88fbe6b7b8303c2e1b485a2
timvisee <tim@visee.me>
2023-05-01 11:42:21 +0200

    Group imports

--- quantization/src/lib.rs
+++ quantization/src/lib.rs
@@ -6,22 +6,12 @@ pub mod kmeans;
 pub mod quantile;
 
 use std::fmt::Display;
-use std::sync::Arc;
-use std::sync::Condvar;
-use std::sync::Mutex;
+use std::sync::{Arc, Condvar, Mutex};
 
-pub use encoded_vectors::DistanceType;
-pub use encoded_vectors::EncodedVectors;
-pub use encoded_vectors::VectorParameters;
-
-pub use encoded_storage::EncodedStorage;
-pub use encoded_storage::EncodedStorageBuilder;
-
-pub use encoded_vectors_u8::EncodedQueryU8;
-pub use encoded_vectors_u8::EncodedVectorsU8;
-
-pub use encoded_vectors_pq::EncodedQueryPQ;
-pub use encoded_vectors_pq::EncodedVectorsPQ;
+pub use encoded_storage::{EncodedStorage, EncodedStorageBuilder};
+pub use encoded_vectors::{DistanceType, EncodedVectors, VectorParameters};
+pub use encoded_vectors_pq::{EncodedQueryPQ, EncodedVectorsPQ};
+pub use encoded_vectors_u8::{EncodedQueryU8, EncodedVectorsU8};
 
 #[derive(Debug)]
 pub struct EncodingError {
