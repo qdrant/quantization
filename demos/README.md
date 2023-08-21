af234490d75c9d02e43e1de4f0a7bf66e6dfd0c6
Andrey Vasnetsov <andrey@vasnetsov.com>
2023-08-21 10:08:13 +0200

    implement binary quantization (#18)
    * implement binary quantization
    
    * fmt
    
    * minor changes
    
    * unit tests
    
    ---------
    
    Co-authored-by: Ivan Pleshkov <pleshkov.ivan@gmail.com>

---
+++ demos/README.md
@@ -0,0 +1,12 @@
+
+
+
+## Installation
+
+Requires HDF5 library for running demo. Demo downloads and extracts HDF5 file from `https://ann-benchmarks.com/`
+
+Ubuntu:
+
+```bash
+sudo apt-get install libhdf5-dev
+```
--- quantization/src/lib.rs
+++ quantization/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod encoded_storage;
 pub mod encoded_vectors;
+pub mod encoded_vectors_binary;
 pub mod encoded_vectors_pq;
 pub mod encoded_vectors_u8;
 pub mod kmeans;
 pub mod quantile;
+mod utils;
 
 use std::fmt::Display;
 use std::sync::{Arc, Condvar, Mutex};
