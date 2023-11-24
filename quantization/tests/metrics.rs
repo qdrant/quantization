694abe199eb3f9867eb9ac5dea7b531b5cb45bec
timvisee <tim@visee.me>
2023-11-24 09:49:56 +0100

    Remove redundant copies

--- quantization/tests/metrics.rs
+++ quantization/tests/metrics.rs
@@ -3,15 +3,9 @@ pub fn dot_similarity(v1: &[f32], v2: &[f32]) -> f32 {
 }
 
 pub fn l2_similarity(v1: &[f32], v2: &[f32]) -> f32 {
-    v1.iter()
-        .zip(v2.iter().copied())
-        .map(|(a, b)| (a - b).powi(2))
-        .sum()
+    v1.iter().zip(v2).map(|(a, b)| (a - b).powi(2)).sum()
 }
 
 pub fn l1_similarity(v1: &[f32], v2: &[f32]) -> f32 {
-    v1.iter()
-        .zip(v2.iter().copied())
-        .map(|(a, b)| (a - b).abs())
-        .sum()
+    v1.iter().zip(v2).map(|(a, b)| (a - b).abs()).sum()
 }
